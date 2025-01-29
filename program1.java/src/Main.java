c5a90036a0840c001f35eb439e2d16f798c88091
UnDeAdIzanagi <britt.m.bradley@gmail.com>
2025-01-28 22:05:45 -0700

    Add files via upload

---
+++ program1.java/src/Main.java
@@ -0,0 +1,39 @@
+//Britt Bradley, writing a verse and giving the title and author.
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\tLike Janis (song) by Sixto Rodrígues\n");
+
+        //Verse 1
+        System.out.println("\tAnd you measure for wealth\n" +
+                "\tby the thing you can hold");
+
+        System.out.println("\tAnd you measure for love\n" +
+                "\tby the sweet things you're told");
+
+        System.out.println("\tAnd you live in the past\n" +
+                "\tof a dream that you're in");
+
+        System.out.println("\tAnd your selfishness\n" +
+                "\tis your card");
+
+        //space between verses
+        System.out.println("");
+
+        //Verse 2
+        System.out.println("\tAnd you want to be held\n" +
+                "\twith highest regard");
+
+        System.out.println("\tIt delight you so much");
+
+        System.out.println("\tIf he's trying so hard");
+
+        System.out.println("\tAnd you're trying to conceal\n" +
+                "\tyour ordinary way");
+
+        System.out.println("\tWith a smile or a shrug");
+
+        System.out.println("\tOr some stolen cliché");
+
+    }
+}
